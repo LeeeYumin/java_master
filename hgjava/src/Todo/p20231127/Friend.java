f54083a19bd5a59329cd873246de3b35b38e9aeb
Administrator <Administrator@DESKTOP-HONP501>
2023-11-28 00:24:34 +0900

    오류찾는다고못함

--- hgjava/src/Todo/p20231127/Friend.java
+++ hgjava/src/Todo/p20231127/Friend.java
@@ -3,8 +3,8 @@ package Todo.p20231127;
 public class Friend {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//  친구의 정보를 이름/연락처/혈액형 입력할 수 있도록 선언(Friend)
 
-	}
+	}//
 
-}
+}//
--- hgjava/src/Todo/p20231127/FriendApp.java
+++ hgjava/src/Todo/p20231127/FriendApp.java
@@ -3,8 +3,8 @@ package Todo.p20231127;
 public class FriendApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 친구의 정보를 입력하는 app
+		// 친구의 정보를 1. 친구수 2. 입력 3. 목록 4. 수정 5. 종료(FriendApp) Public class FriendApp
+	}//
 
-	}
-
-}
+}//
